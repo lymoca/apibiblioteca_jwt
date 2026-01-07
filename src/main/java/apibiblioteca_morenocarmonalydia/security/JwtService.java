3df1a6b1746e72565d733c775eb55616032e26c1
Lydia <morenocarmonalydia@gmail.com>
2026-01-07 21:19:13 +0100

    Correcciones JWT

--- src/main/java/apibiblioteca_morenocarmonalydia/security/JwtService.java
+++ src/main/java/apibiblioteca_morenocarmonalydia/security/JwtService.java
@@ -1,6 +1,5 @@
 package apibiblioteca_morenocarmonalydia.security;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -9,11 +8,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Getter
